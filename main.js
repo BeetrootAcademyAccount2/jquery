cd126689ece597297c0c22afb22a31aff9a21f1e
BeetrootAcademyAccount2 <beetroot444@yahoo.com>
2024-04-18 22:14:20 +0300

    Add files

---
+++ main.js
@@ -0,0 +1,104 @@
+let drinks = [
+  { name: "Beer", price: 2 },
+  { name: "Whiskey", price: 5 },
+  { name: "Wine", price: 8 },
+];
+
+// $(document).ready(function () {
+//   $("#btn").click(function () {
+//     console.log("Clicked");
+//     $("#title").hide();
+//   });
+// });
+
+$(() => {
+  // drinks.forEach((value, i)=> console.log(i, value.name))
+
+  $.each(drinks, (i, value) => {
+    const li = $(`<li>${i + 1}.${value.name}</li>`);
+    $("ul").append(li);
+  });
+
+  $("#btnToggle").on("click", () => {
+    console.log("Clicked");
+    $(".img").toggle(1000);
+  });
+
+  $("#btnShow").on("click", () => {
+    console.log("Clicked");
+    $(".img").show(2000);
+  });
+
+  $("#btnHide").on("click", () => {
+    console.log("Clicked");
+    $(".img").hide(5000);
+  });
+
+  $("#btnStop").on("click", () => {
+    console.log("Clicked");
+    $(".img").stop(true, true);
+  });
+
+  $("#btnCss").on("click", () => {
+    console.log("Clicked");
+    // $("h1").css("color", "red");
+    // $("h1").css("background-color", "blue");
+    $("h1").toggleClass("titleStyle");
+  });
+
+  $("#btnChangeInput").on("click", () => {
+    // $("#randomInput").attr("type", "password");
+    // $("#randomInput").val("random");
+  });
+
+  $("#btnChangeTitle").on("click", () => {
+    // $("#randomInput").attr("type", "password");
+    // $("#title").text("New title");
+    $("#title").html("<h2>New title</h2>");
+  });
+
+  $("#btnAddTitle").on("click", () => {
+    $("#empty").html("<h2>New title</h2>");
+  });
+
+  $("#btnSlideUp").on("click", () => {
+    $("img").slideUp(2000);
+  });
+
+  $("#btnSlideDown").on("click", () => {
+    $("img").slideDown(2000);
+  });
+
+  $("#btnSlideToggle").on("click", () => {
+    $("img").slideToggle(2000);
+  });
+
+  $("#btnFadeIn").on("click", () => {
+    $("img").fadeIn(2000);
+  });
+
+  $("#btnFadeOut").on("click", () => {
+    $("img").fadeOut(2000);
+  });
+
+  $("#btnFadeToggle").on("click", () => {
+    $("img").fadeToggle(2000);
+  });
+
+  $("#btnAnimate").on("click", () => {
+    $("img").animate(
+      {
+        width: "800px",
+        opacity: 0.4,
+      },
+      3000
+    );
+  });
+});
+
+// const titleElement = document.getElementById("title");
+// const btnElement = document.getElementById("btn");
+
+// btnElement.addEventListener("click", () => {
+//   titleElement.style.display = "none";
+// });
